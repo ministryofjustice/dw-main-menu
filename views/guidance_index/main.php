6b404538ddc3f1c871152093127348cc160ab240
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-12-03 15:20:32 +0000

    updated class name of guidance category element

--- views/guidance_index/main.php
+++ views/guidance_index/main.php
@@ -3,14 +3,14 @@
 <div style="clear: both">
 </div>
 <div class="guidance-index-widget">
-  <h2 class="category">Most visited</h2>
+  <h2 class="guidance-category">Most visited</h2>
   <ul class="guidance-categories-list large">
     <?php foreach($menu_data['large_menu'] as $category): ?>
       <?php $this->view('guidance_index/category_item', $category) ?>
     <?php endforeach ?>
   </ul>
 
-  <h2 class="category all-items">All</h2>
+  <h2 class="guidance-category all-items">All</h2>
   <ul class="guidance-categories-list small">
     <?php foreach($menu_data['small_menu'] as $category): ?>
       <?php $this->view('guidance_index/category_item', $category) ?>
